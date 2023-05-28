0522fcc0f0d72de7e780044b72dc952502c40162
SaulTU <sto.jur.st@gmail.com>
2023-05-28 16:00:48 -0600

    Metodos de lista

---
+++ 7_Lists/3_MetodosListas.py
@@ -0,0 +1,27 @@
+lista=[1,2,3,4,5]
+#Agregar datos
+lista.append(6)
+print(lista)
+#por posicion
+lista.insert(2,'python')
+print(lista)
+#cuenta el numero de repeticiones
+print(lista.count(5))
+#indica la posicion
+print(lista.index('python'))
+lista=[5,3,1,2,4]
+#ordena lista
+lista.sort()
+print(lista)
+#ordena al revez
+lista.reverse()
+print(lista)
+lista=['python',1,'rene','alexander',12]
+print(lista)
+lista[3]='Alexander'
+print(lista)
+#elimina el ultimo dato
+lista.pop()
+print(lista)
+lista.remove(1)
+print(lista)
