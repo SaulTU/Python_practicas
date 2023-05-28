04c7ad745fcbc4652dbe40a1f33f48e54d460599
SaulTU <sto.jur.st@gmail.com>
2023-05-28 16:01:40 -0600

    Ejercicios de lista

---
+++ 7_Lists/5_ejercicio1.py
@@ -0,0 +1,7 @@
+lista = [20, 50, "Curso", 'Python', 3.14]
+print(lista)
+a=int(input("Ingresa Dato 1 "))
+b=int(input("Ingresa Dato 2 "))
+lista[0]=a
+lista[1]=b
+print(lista)
---
+++ 7_Lists/6_ejercicio2.py
@@ -0,0 +1,9 @@
+lista = [1,2,3,4,5,6,7,8,9,10]
+print(lista)
+a=lista[3]*2
+b=lista[6]*2
+c=lista[8]*2
+lista.append(a)
+lista.append(b)
+lista.append(c)
+print(lista)
