2f8a01cf93a22a71f6cf6694f345d8a49a8d2ab7
SaulTU <sto.jur.st@gmail.com>
2023-05-28 14:25:17 -0600

    Metodos de python para cadenas

---
+++ 3_Strings/4_MetodosCaracter.py
@@ -0,0 +1,9 @@
+cadena = 'Estoy utilizando los metodos de python'
+
+print(cadena.lower())
+print(cadena.upper())
+#Oracion
+print(cadena.capitalize())
+print(cadena.title())
+#mayusculas en minusculas y viseversa
+print(cadena.swapcase())
