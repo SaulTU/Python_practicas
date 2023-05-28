42452d955621a1009c866d97cac8988fccef1de5
SaulTU <sto.jur.st@gmail.com>
2023-05-28 14:23:33 -0600

    Sintaxis adicionales a una cadena

---
+++ 3_Strings/1_Strings.py
@@ -0,0 +1,10 @@
+cadena = "Esta \f \v es una \"cadena\" genial"
+'''
+\n salto de linea
+\t tabulacion
+\b quita espacio
+\f coloca un simbolo femenino
+\v coloca un simbolo masculino
+\r borra
+'''
+print(cadena)
