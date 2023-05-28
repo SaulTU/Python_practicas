9df9f02c10e5179829631b6eebc130a053ded789
SaulTU <sto.jur.st@gmail.com>
2023-05-28 15:04:10 -0600

    Metodos booleanos

---
+++ 5_Boolean/4_MetodosBooleanos.py
@@ -0,0 +1,11 @@
+cadena = "Estoy mostrando los metodos booleanos para las Strings"
+cadena2 = "CMSJDNCFISDNFISDNCINXICNDIUNSIJND"
+cadena3 = "cmindcvndsifdnfijofnisncnsmed"
+#La letra con la que empieza la cadena
+print(cadena.startswith("e"))
+#Si finaliza con una letra
+print(cadena.endswith("S"))
+#toda la cadena es mayuscula
+print(cadena2.isupper())
+#toda es minuscula
+print(cadena3.islower())
