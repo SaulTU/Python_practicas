5dd714fa64f50bbbfa6c3c5bc7295f9919e0b48d
SaulTU <sto.jur.st@gmail.com>
2023-05-28 13:34:06 -0600

    Un indicativo de la forma de documentar tu codigo y declaracion de varianles constantes

---
+++ 1_Introduccion/3_sintaxis.py
@@ -0,0 +1,21 @@
+cantidadPersonas = 100
+
+print(cantidadPersonas)
+
+cantidadPersonas = 150
+
+print(cantidadPersonas)
+
+#constantes en Mayusculas, consejo de programador
+PI = 3.14
+print(PI)
+
+#comentarios con el gato 
+
+'''
+comentario de multiples lineas
+con 3 comillas simples
+'''
+nombre, apellido = 'Saul', 'Torres'
+print(nombre)
+print(apellido)
