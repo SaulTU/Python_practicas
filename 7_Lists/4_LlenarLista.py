5c8ecfed39818af220df0435e94f558b0df20aba
SaulTU <sto.jur.st@gmail.com>
2023-05-28 16:01:16 -0600

    Ingresar datos a lista

---
+++ 7_Lists/4_LlenarLista.py
@@ -0,0 +1,15 @@
+lista1=[1,2,3]
+lista2=[4,5,6]
+
+lista3= lista1+lista2
+print(lista3)
+
+print('Lista de distintos datos',lista1)
+
+listaV=[]
+
+print(type(listaV))
+
+edad=int(input("Ingresa edad "))
+listaV.append(edad);
+print(listaV)
