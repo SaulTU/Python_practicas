c34a7b785ebb25731ff2b65ab60dab483cf1b429
SaulTU <sto.jur.st@gmail.com>
2023-05-28 14:26:11 -0600

    Ejercicio 1 una de muchas soluciones

---
+++ 3_Strings/5_ejercicio1.py
@@ -0,0 +1,10 @@
+cadena = "Te quiero solo como amigo"
+#los dos primeros caracteres
+print(cadena[:2])
+#los 3 ultimos caracteres
+print(cadena[-3:])
+#cada dos caracteres
+print(cadena[: :2])
+#inversa
+print(cadena[: : -1])
+print(cadena[0:]+cadena[: : -1])
