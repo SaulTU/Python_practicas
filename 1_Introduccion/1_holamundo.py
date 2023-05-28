0c9ce99141a287673bc7f053d7aff62fa7a19543
SaulTU <sto.jur.st@gmail.com>
2023-05-28 13:10:22 -0600

    Declaracion de variables, mostrar en pantalla tipo de dato y dato

--- 1_Introduccion/1_holamundo.py
+++ 1_Introduccion/1_holamundo.py
@@ -1,1 +1,12 @@
-print("hola mundo")
+nombre = "Saul TU"
+numero = 23
+flotante = 9.81
+print(type(nombre))
+print(nombre)
+
+print(type(numero))
+print(numero)
+
+
+print(type(flotante))
+print(flotante)
