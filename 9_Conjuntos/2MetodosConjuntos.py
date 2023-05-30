0998a0f7f2513db2370d18e914e4110e62c62864
SaulTU <sto.jur.st@gmail.com>
2023-05-29 23:06:55 -0600

    Metodos de conjuntos

---
+++ 9_Conjuntos/2MetodosConjuntos.py
@@ -0,0 +1,16 @@
+conjunto = {1,1,2,2,3,4,5}
+print(conjunto)
+#añade al conjunto
+conjunto.add(20)
+print(conjunto)
+#elimina un valor
+conjunto.remove(1)
+print(conjunto)
+#elimina 
+print(conjunto.pop())
+#añade
+conjunto.update([10,20,30])
+print(conjunto)
+#limpia
+conjunto.clear()
+print(conjunto)
