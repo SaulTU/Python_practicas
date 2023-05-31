bba5c808306e375a64b430fc6a7d315edd19f9e8
SaulTU <sto.jur.st@gmail.com>
2023-05-30 23:38:29 -0600

    Uso de for

---
+++ 11_Bucles/5range.py
@@ -0,0 +1,11 @@
+lista =['uno','dos','tres']
+tupla = 1,2,3,4,5
+
+for i in range(1,10,2):
+    print(i)
+print("")
+for i in range(1,10):
+    print(i)
+print("")
+for i in range(-10,-1):
+    print(i)
