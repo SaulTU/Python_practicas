1d28e75f1c7bbe87227838ca9ccdb206a5bc24c3
SaulTU <sto.jur.st@gmail.com>
2023-05-30 23:37:50 -0600

    Uso de while

---
+++ 11_Bucles/2ejercicio1.py
@@ -0,0 +1,5 @@
+a = int(input("ingresa un numero: "))
+i=0
+while i<=10:
+    print("{} x {} = {}".format(a,i,a*i))
+    i+=1
---
+++ 11_Bucles/3ejercicio2.py
@@ -0,0 +1,6 @@
+a = int(input("ingresa su edad: "))
+i=0
+while i<a:
+    i+=1
+    print("Haz cumplido {}".format(i))
+    
