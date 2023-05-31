85078384766177d591788ccf0a28a96ecc044597
SaulTU <sto.jur.st@gmail.com>
2023-05-30 23:38:46 -0600

    Ejercicios de For

---
+++ 11_Bucles/7ejercicio3.py
@@ -0,0 +1,9 @@
+for i in range(10):
+    print(i+1)
+
+a=int(input('Primer numero: '))
+b=int(input('Segundo numero: '))
+
+while a<=b:
+    print(a)
+    a+=1
---
+++ 11_Bucles/8ejercicio4.py
@@ -0,0 +1,7 @@
+a=int(input('Primer numero: '))
+b=int(input('Segundo numero: '))
+i=a
+while i<=b:
+    if i%2!=0:
+        print(i)
+    i+=1
