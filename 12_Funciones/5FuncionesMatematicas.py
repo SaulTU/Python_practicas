22e33a25a911869f21cb75fc78e0c84e0eba502d
SaulTU <sto.jur.st@gmail.com>
2023-05-31 00:34:39 -0600

    Funciones matematicas

---
+++ 12_Funciones/5FuncionesMatematicas.py
@@ -0,0 +1,18 @@
+import math
+import random
+#elevacion exponencial
+print(pow(10 , 2))
+#raiz cuadrada float
+print(math.sqrt(64))
+#raiz cuadrada int
+print(math.isqrt(64))
+#seno
+print(math.sin(90))
+#coseno
+print(math.cos(90))
+#tangente
+print(math.tan(90))
+#factorial
+print(math.factorial(10))
+#numero random
+print(random.randint(1 , 100))
